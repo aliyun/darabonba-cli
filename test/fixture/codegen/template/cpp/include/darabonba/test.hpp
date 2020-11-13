1262868296b21d1decd710eba616575eb7fb7b7d
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-11-13 17:17:28 +0800

    test: update test case

---
+++ test/fixture/codegen/template/cpp/include/darabonba/test.hpp
@@ -0,0 +1,20 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+#ifndef DARABONBA_TEST_H_
+#define DARABONBA_TEST_H_
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+namespace Darabonba_Test {
+class Client {
+public:
+  static map<string, string> helloMap();
+
+  Client() {};
+};
+} // namespace Darabonba_Test
+
+#endif
