730fd85efef28b4456e195740fd66afb224bb8c9
Jackson Tian <shyvo1987@gmail.com>
2023-12-04 10:57:26 +0800

    test: cleanup dependences

--- test/lib/helper.test.js
+++ test/lib/helper.test.js
@@ -2,26 +2,26 @@
 
 const assert = require('assert');
 const path = require('path');
-const rewire = require('rewire');
 
-const helper = rewire('../../lib/helper');
+const {
+  _upperFirst,
+  getDarafile,
+  supportedLang
+} = require('../../lib/helper');
 
 describe('helper lib should ok', function () {
   it('_upperFirst should be ok', async function () {
-    const _upperFirst = helper.__get__('_upperFirst');
     assert.deepStrictEqual(_upperFirst('test'), 'Test');
     assert.deepStrictEqual(_upperFirst('test-string'), 'Test_string');
   });
 
   it('getDarafile should be ok', async function () {
-    const getDarafile = helper.__get__('getDarafile');
     assert.deepStrictEqual(getDarafile(__dirname), path.join(__dirname, 'Darafile'));
     const TeaDirPath = path.join(__dirname,'../fixture/helper');
     assert.deepStrictEqual(getDarafile(TeaDirPath), path.join(TeaDirPath, 'Teafile'));
   });
 
   it('supportedLang should be ok', async function () {
-    const supportedLang = helper.__get__('supportedLang');
     const codegen = {
       ts: ['typescript'],
       cs: ['csharp', 'net'],
